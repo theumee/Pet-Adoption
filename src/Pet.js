f906e2af7f20c6a0cfecc40178149bfcb37c5c81
theumee <theumar01@gmail.com>
2021-10-19 21:37:01 +0530

    ignore

---
+++ src/Pet.js
@@ -0,0 +1,11 @@
+const Pet = (props) => {
+  return (
+    <div id="blah">
+      <h2>{props.name}</h2>
+      <h3>{props.animal}</h3>
+      <h3>{props.breed}</h3>
+    </div>
+  );
+};
+
+export default Pet;
