d3bd48d711823f8b6d4365487ea9edba8edc60f6
theumee <theumar01@gmail.com>
2022-01-02 21:22:18 +0530

    multiple

---
+++ src/Details.js
@@ -0,0 +1,54 @@
+import { Component } from "react";
+import { withRouter } from "react-router-dom";
+import Carousel from "./Carousel";
+
+class Details extends Component {
+  state = { loading: true };
+
+  async componentDidMount() {
+    const res = await fetch(
+      `http://pets-v2.dev-apis.com/pets?id=${this.props.match.params.id}`
+    );
+    const json = await res.json();
+    this.setState(
+      Object.assign(
+        {
+          loading: false,
+        },
+        json.pets[0]
+      )
+    );
+    // this.setState({
+    //   loading: false,
+    //   name: json.pets[0].name,
+    //   breed: json.pets[0].breed,
+    //   animal: json.pets[0].animal,
+    //   city: json.pets[0].city,
+    //   state: json.pets[0].state,
+    //   description: json.pets[0].description,
+    //   images: json.pets[0].images,
+    // });
+  }
+  render() {
+    if (this.state.loading) {
+      return <h2>Loading.....</h2>;
+    }
+    const { animal, breed, city, state, description, name, images } =
+      this.state;
+    return (
+      <div className="details">
+        <Carousel images={images} />
+        <div>
+          <h1>{name}</h1>
+          <h2>
+            {animal} - {breed} - {city}, {state}
+          </h2>
+          <button>Adopt {name}</button>
+          <p>{description}</p>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default withRouter(Details);
--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,32 +1,38 @@
 import { useState, useEffect } from "react";
-import Pet from "./Pet";
 import useBreedList from "./useBreedList";
+import Results from "./Result";
 
 const ANIMALS = ["bird", "cat", "dog", "rabbit", "monkey"];
 
 const SearchParams = () => {
-  const [location, setLocation] = useState("Seattle, WA");
+  const [location, setLocation] = useState("");
   const [animal, setAnimal] = useState("");
   const [breed, setBreed] = useState("");
   const [pets, setPets] = useState([]);
   const [breeds] = useBreedList(animal);
 
   useEffect(() => {
+    /** used to get "side, effects" */
     requestPets();
   }, []); //eslint-disable-line react-hooks/exhaustive-deps
 
-  async function requestPets() {
+  const requestPets = async () => {
     const res = await fetch(
       `https://pets-v2.dev-apis.com/pets?animal=${animal}&location=${location}&breed=${breed}`
     );
     const json = await res.json();
 
     setPets(json.pets);
-  }
+  };
 
   return (
     <div className="search-params">
-      <form>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          requestPets();
+        }}
+      >
         <label htmlFor="location">
           Location
           <input
@@ -70,14 +76,7 @@ const SearchParams = () => {
         </label>
         <button>Submit</button>
       </form>
-      {pets.map((pet) => (
-        <Pet
-          name={pet.name}
-          animal={pet.animal}
-          breed={pet.breed}
-          key={pet.id}
-        />
-      ))}
+      <Results pets={pets} />
     </div>
   );
 };
